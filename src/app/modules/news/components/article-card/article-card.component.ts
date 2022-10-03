0c884216ac00451c20cf5a2e7feaeba928a77a17
aeserein <ae.serein@gmail.com>
2022-10-03 17:27:29 -0300

    Overlay en imágenes de artículos

--- src/app/modules/news/components/article-card/article-card.component.ts
+++ src/app/modules/news/components/article-card/article-card.component.ts
@@ -1,4 +1,4 @@
-import { state, style, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { News } from '../../classes/news';
 
@@ -11,7 +11,29 @@ import { News } from '../../classes/news';
 			state("hover", style({
 				color: "#00558E"
 			})),
-		])
+		]),
+		trigger("imageOverlay", [
+			state("hover", style({
+				opacity: 0.7
+			})),
+			state("normal", style({
+				opacity: 0
+			})),
+			transition("normal => hover", animate("0.3s ease")),
+			transition("hover => normal", animate("0.3s ease-out")),
+		]),
+		trigger("readMore", [
+			state("hover", style({
+				transform: "translateY(-10px)",
+				opacity: 1
+			})),
+			state("normal", style({
+				transform: "none",
+				opacity: 0
+			})),
+			transition("normal => hover", animate("0.5s ease")),
+			transition("hover => normal", animate("0.5s ease-out")),
+		]),
 	]
 })
 export class ArticleCardComponent {
